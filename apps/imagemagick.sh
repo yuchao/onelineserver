fddd4a70f18df8d60d2fa27f82ba4deba9bbba71
Dean Perry <deanpcmad@gmail.com>
2012-06-27 14:05:19 +0100

    updated libmagick9-dev to libmagickwand-dev

--- apps/imagemagick.sh
+++ apps/imagemagick.sh
@@ -4,7 +4,7 @@
 sudo -v >/dev/null 2>&1 || { echo $(whoami) has no sudo privileges ; exit 1; }
 
 # Install ImageMagick
-sudo apt-get -y install imagemagick libmagick9-dev
+sudo apt-get -y install imagemagick libmagickwand-dev
 
 echo "###############################################"
 echo "     imagemagick installation is complete!     "
